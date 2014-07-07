2cd02ae7c64a4755e54536a3a1f5332d40d9236f
Kyle Diedrick <corporealshift@gmail.com>
2014-07-06 18:45:51 -0700

    Add some things to the Task model. Make the url for tasks.

--- server/app/models/Task.scala
+++ server/app/models/Task.scala
@@ -3,14 +3,15 @@ package models
 import play.api.db.slick.Config.driver.simple._
 import java.sql.Date
 
-case class Task(id: Int, title: String, description: String, due_date: Date, points: Int)
+case class Task(id: Int, title: String, description: String, due_date: Date, points: Int, repeats: String)
 
 class TaskTable(tag: Tag) extends Table[Task] (tag, "TASK") {
   def id          = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def title       = column[String]("title", O.NotNull)
   def description = column[String]("description")
-  def due_date    = column[Date]("due_date", O.DBType("Date"))
+  def due_date    = column[Date]("due_date", O.Nullable)
   def points      = column[Int]("points", O.NotNull)
+  def repeats     = column[String]("repeats", O.Nullable)
 
-  def * = (id, title, description, due_date, points) <> (Task.tupled, Task.unapply)
+  def * = (id, title, description, due_date, points, repeats) <> (Task.tupled, Task.unapply)
 }
--- server/conf/evolutions/default/1.sql
+++ server/conf/evolutions/default/1.sql
@@ -4,7 +4,7 @@
 # --- !Ups
 
 create table "CAT" ("name" VARCHAR(254) NOT NULL PRIMARY KEY,"color" VARCHAR(254) NOT NULL);
-create table "TASK" ("id" SERIAL NOT NULL PRIMARY KEY,"title" VARCHAR(254) NOT NULL,"description" VARCHAR(254) NOT NULL,"due_date" Date NOT NULL,"points" INTEGER NOT NULL);
+create table "TASK" ("id" SERIAL NOT NULL PRIMARY KEY,"title" VARCHAR(254) NOT NULL,"description" VARCHAR(254) NOT NULL,"due_date" DATE,"points" INTEGER NOT NULL,"repeats" VARCHAR(254));
 
 # --- !Downs
 
