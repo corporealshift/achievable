ddcf9983f0a76bb48d3ea27efcd8cd154f1ae7c6
Kyle Diedrick <corporealshift@gmail.com>
2014-07-08 22:37:05 -0700

    Add tasks and display all tasks workinggit add .git add .git add .git add .

--- server/app/controllers/TaskController.scala
+++ server/app/controllers/TaskController.scala
@@ -1,5 +1,7 @@
 package controllers
 
+import java.sql.Date
+
 import models._
 import play.api._
 import play.api.db.slick._
@@ -11,6 +13,9 @@ import play.api.Play.current
 import play.api.mvc.BodyParsers._
 import play.api.libs.json.Json
 import play.api.libs.json.Json._
+import play.api.mvc._
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 object TaskController extends Controller {
 
@@ -24,9 +29,34 @@ object TaskController extends Controller {
     Ok(toJson(tasks.filter{_.id == id}.firstOption))
   }
 
-//  def insert = DBAction { implicit rs =>
-//    val task = ???
-//  }
+  val task_form = Form(
+    mapping(
+      "id" -> optional(number),
+      "title" -> text(),
+      "description" -> optional(text),
+      "due_date" -> optional(sqlDate),
+      "points" -> number(),
+      "repeats" -> optional(text)
+    )(Task.apply)(Task.unapply)
+  )
+
+  def insert = DBAction(BodyParsers.parse.json) { implicit rs =>
+    val taskResult = rs.request.body.validate[Task]
+    taskResult.fold(
+      errors => {
+        BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toFlatJson(errors)))
+      },
+      task => {
+        tasks.insert(task)
+        Ok(Json.obj("status" ->"OK", "message" -> ("Title '"+task.title+"' saved.") ))
+      }
+    )
+
+//    rs.request.body.validate[Task].map { task =>
+//      tasks.insert(task)
+//      Ok(toJson(task))
+//    }.getOrElse(BadRequest("invalid json"))
+  }
 
   //  def insert = DBAction { implicit rs =>
   //    val cat = catForm.bindFromRequest.get
--- server/app/models/Task.scala
+++ server/app/models/Task.scala
@@ -3,15 +3,15 @@ package models
 import play.api.db.slick.Config.driver.simple._
 import java.sql.Date
 
-case class Task(id: Int, title: String, description: String, due_date: Date, points: Int, repeats: String)
+case class Task(id: Option[Int], title: String, description: Option[String], due_date: Option[Date], points: Int, repeats: Option[String])
 
 class TaskTable(tag: Tag) extends Table[Task] (tag, "TASK") {
   def id          = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def title       = column[String]("title", O.NotNull)
-  def description = column[String]("description")
+  def description = column[String]("description", O.Nullable)
   def due_date    = column[Date]("due_date", O.Nullable)
   def points      = column[Int]("points", O.NotNull)
   def repeats     = column[String]("repeats", O.Nullable)
 
-  def * = (id, title, description, due_date, points, repeats) <> (Task.tupled, Task.unapply)
+  def * = (id.?, title, description.?, due_date.?, points, repeats.?) <> (Task.tupled, Task.unapply)
 }
